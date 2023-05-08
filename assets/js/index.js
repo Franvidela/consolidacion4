b57d09e9b1ad2bad6493bca281fef6ab1986f207
Fran Videla Mardones <franvidelamardones@gmail.com>
2023-05-08 11:05:38 -0400

    otro mas

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,14 @@
 $(() => {
 	class Personaje {
-		constructor(nombre, estatura, peso) {
+		constructor(nombre, estatura, peso, fila, color) {
 			this.nombre = nombre;
 			this.estatura = estatura;
 			this.peso = peso;
+			this.fila = fila;
+			this.color = color;
 		}
-	}
-
-	const obtenerPersonaje = async (id, row, color) => {
-		try {
-			let response = await fetch(`https://swapi.dev/api/people/${id}`);
-			let personaje = await response.json();
-			generarPersonaje(personaje, row, color);
-			console.log(color);
-		} catch (error) {
-			console.log(error);
-		}
-	};
-	const generarPersonaje = (personaje, row, color) => {
-		let char = new Personaje(personaje.name, personaje.height, personaje.mass);
-		console.log(char);
-		generarCard(char, row, color);
-	};
-	const generarCard = (pers, row, color) => {
-		console.log(color);
-		$(row).append(`
+		generarCard = () => {
+			$(this.fila).append(`
 	    <div class="col-12 col-md-6 col-lg-4">
 	        <div
 	            class="single-timeline-content d-flex wow fadeInLeft 2021"
@@ -37,21 +21,43 @@ $(() => {
 	        >
 	            <div
 	                class="timeline-icon"
-	                style="background-color: ${color}"
+	                style="background-color: ${this.color}"
 	            >
 	                <i class="fa fa-address-card" aria-hidden="true"></i>
 	            </div>
 	            <div class="timeline-text">
-	                <h6>${pers.nombre}</h6>
+	                <h6>${this.nombre}</h6>
 	                <p>
-	                    Altura: ${pers.estatura}cm. Peso: ${pers.peso}kg
+	                    Altura: ${this.estatura}cm. Peso: ${this.peso}kg
 	                </p>
 	            </div>
 	            </div>
 	        </div>
 	    </div>
 	    `);
+		};
+	}
+
+	const obtenerPersonaje = async (id, row, color) => {
+		try {
+			let response = await fetch(`https://swapi.dev/api/people/${id}`);
+			let personaje = await response.json();
+			generarPersonaje(personaje, row, color);
+		} catch (error) {
+			console.log(error);
+		}
 	};
+	const generarPersonaje = (personaje, row, color) => {
+		let char = new Personaje(
+			personaje.name,
+			personaje.height,
+			personaje.mass,
+			row,
+			color
+		);
+		char.generarCard();
+	};
+
 	function* generador1(i, row, color) {
 		yield obtenerPersonaje(i, row, color);
 		i++;
@@ -67,94 +73,15 @@ $(() => {
 	let gen2 = generador1(6, '.secondRow', 'lightgreen');
 	let gen3 = generador1(11, '.thirdRow', 'lightskyblue');
 	$('#generador1').on('click', () => {
-		// !gen1.next().done ? gen1.next() : console.log('No hay mas personajes');
-		gen1.next();
+		let data = gen1.next();
+		!data.done ? data.value : alert('No hay mas personajes por mostrar');
 	});
 	$('#generador2').on('click', () => {
-		// !gen2.next().done ? gen1.next() : console.log('No hay mas personajes');
-		gen2.next();
+		let data = gen2.next();
+		!data.done ? data.value : alert('No hay mas personajes por mostrar');
 	});
 	$('#generador3').on('click', () => {
-		// !gen3.next().done ? gen1.next() : console.log('No hay mas personajes');
-		gen3.next();
+		let data = gen3.next();
+		!data.done ? gen1.value : alert('No hay mas personajes por mostrar');
 	});
-	// class Personaje {
-	// 	constructor(nombre, estatura, peso) {
-	// 		this.nombre = nombre;
-	// 		this.estatura = estatura;
-	// 		this.peso = peso;
-	// 	}
-	// }
-
-	// const obtenerPersonaje = async (id, row, color) => {
-	// 	try {
-	// 		let response = await fetch(`https://swapi.dev/api/people/${id}`);
-	// 		let personaje = await response.json();
-	// 		return personaje;
-	// 	} catch (error) {
-	// 		console.log(error);
-	// 	}
-	// };
-	// const generarPersonaje = (personaje, row, color) => {
-	// 	let char = new Personaje(personaje.name, personaje.height, personaje.mass);
-	// 	console.log(char);
-	// 	generarCard(char, row, color);
-	// };
-	// const generarCard = (pers, row, color) => {
-	// 	console.log(color);
-	// 	$(row).append(`
-	//     <div class="col-12 col-md-6 col-lg-4">
-	//         <div
-	//             class="single-timeline-content d-flex wow fadeInLeft 2021"
-	//             data-wow-delay="0.3s"
-	//             style="
-	//                 visibility: visible;
-	//                 animation-delay: 0.3s;
-	//                 animation-name: fadeInLeft;
-	//             "
-	//         >
-	//             <div
-	//                 class="timeline-icon"
-	//                 style="background-color: ${color}"
-	//             >
-	//                 <i class="fa fa-address-card" aria-hidden="true"></i>
-	//             </div>
-	//             <div class="timeline-text">
-	//                 <h6>${pers.nombre}</h6>
-	//                 <p>
-	//                     Altura: ${pers.estatura}cm. Peso: ${pers.peso}kg
-	//                 </p>
-	//             </div>
-	//             </div>
-	//         </div>
-	//     </div>
-	//     `);
-	// };
-	// function* generador1(i) {
-	// 	yield i;
-	// 	i++;
-	// 	yield i;
-	// 	i++;
-	// 	yield i;
-	// 	i++;
-	// 	yield i;
-	// 	i++;
-	// 	yield i;
-	// }
-	// let gen1 = generador1(1, '.firstRow', 'salmon');
-	// let gen2 = generador1(6, '.secondRow', 'lightgreen');
-	// let gen3 = generador1(11, '.thirdRow', 'lightskyblue');
-	// $('#generador1').on('click', () => {
-	// 	console.log(gen1.next().value);
-	// });
-	// $('#generador2').on('click', () => {
-	// 	!gen2.next().done
-	// 		? console.log(gen1.next().value)
-	// 		: console.log('No hay mas personajes');
-	// });
-	// $('#generador3').on('click', () => {
-	// 	!gen3.next().done
-	// 		? console.log(gen1.next().value)
-	// 		: console.log('No hay mas personajes');
-	// });
 });
